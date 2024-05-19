ecccad7402c434b1f99a9e1623310720a958a1a1
Denis Trifan <trifan.denis1999@gmail.com>
2024-05-19 17:57:07 +0300

    updated migration

--- src/main/resources/db/migration/V2024_05_19_17_28__Insert_default_admin_Users.sql
+++ src/main/resources/db/migration/V2024_05_19_17_28__Insert_default_admin_Users.sql
@@ -1,9 +1,9 @@
 -- INSERT DEFAULT ADMIN !!!
 -- DEFAULT Account - user: 'admin' password: 'admin' WITHOUT '
 INSERT INTO Users
-(id_institution,idnp,password,email,is_enabled,name,surname,patronymic)
+(idnp,password,email,is_enabled,name,surname,patronymic)
 VALUES
-(NULL,'2000000000000','admin','admin@admin.admin',TRUE,'Admin','Admin','Admin');
+('2000000000000','admin','admin@admin.admin',TRUE,'Admin','Admin','Admin');
 
 INSERT INTO UserToRole
 VALUES
