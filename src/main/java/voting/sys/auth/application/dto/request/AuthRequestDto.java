ef1bfa475b74d7af99e25e3c25a2054b2372ccd0
Denis Trifan <trifan.denis1999@gmail.com>
2024-05-19 17:37:48 +0300

    added auth dto

---
+++ src/main/java/voting/sys/auth/application/dto/request/AuthRequestDto.java
@@ -0,0 +1,22 @@
+package voting.sys.auth.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class AuthRequestDto {
+
+    private String idnp;
+
+    private String password;
+
+}
