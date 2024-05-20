3f850d0d79c088dc67762521c1e9eef26caa44af
Denis Trifan <trifan.denis1999@gmail.com>
2024-05-20 16:00:03 +0300

    added resource exception

---
+++ src/main/java/voting/sys/auth/application/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package voting.sys.auth.application.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
